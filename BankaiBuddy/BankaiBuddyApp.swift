36617fad19de16aa8ccc9e231d5e7fc5484b8c14
TonyB-224 <tbullard224@gmail.com>
2026-04-04 12:24:07 -0500

    Initial Commit

---
+++ BankaiBuddy/BankaiBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankaiBuddyApp.swift
+//  BankaiBuddy
+//
+//  Created by user942908 on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BankaiBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
